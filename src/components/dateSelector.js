a96d2e5328477d0414bbf8a9dbf40d0ebc070398
Matthew Hutter <matthewjhutter@gmail.com>
2024-06-15 16:21:01 -0700

    minor updates to dateSelector and css

--- src/components/dateSelector.js
+++ src/components/dateSelector.js
@@ -4,7 +4,7 @@ import "../css/dateSelector.css"
 const DateSelector = () => {
   return (
     <div className="main-date-selector-div">
-        <select className="date-selector" name="dates">
+        <select className="date-selector" name="dates" id="date-selector">
           <option value="">YEAR</option>
           <option value="1985">1985</option>
           <option value="1995">1995</option>
