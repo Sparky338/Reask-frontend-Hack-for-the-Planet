0b8841a43febb0748631126e2960845ad82d461e
Matthew Hutter <matthewjhutter@gmail.com>
2024-06-15 17:32:36 -0700

    button to upload CSV opens modal, needs to upload to the backend

--- src/components/uploadCSV.js
+++ src/components/uploadCSV.js
@@ -1,21 +1,43 @@
-import "../css/uploadCSV.css"
+import "../css/uploadCSV.css";
 
 const UploadCSV = () => {
+  const uploadIcon = <i class="fa-solid fa-upload"></i>;
 
-    const uploadIcon = <i class="fa-solid fa-upload"></i>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // await => backend call passing in CSV to update data
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // await => backend call passing in CSV to update data
+  const openModal = () => {
+    const modal = document.querySelector("#modal");
+    const openModal = document.querySelector("#openModal");
+    const closeModal = document.querySelector("#closeModal");
+
+    if (modal) {
+      openModal && openModal.addEventListener("click", () => modal.showModal());
+
+      closeModal && closeModal.addEventListener("click", () => modal.close());
     }
+  };
 
-    return (
-        <div className="main-upload-csv-div">
-            <button className="button csv-upload-button" /*onClick={open modal }*/>
-                Upload {uploadIcon}
-            </button>
-        </div>
-    )
-}
+  return (
+    <div className="main-upload-csv-div">
+      <button
+        className="button csv-upload-button"
+        id="openModal"
+        onClick={openModal}
+      >
+        Upload {uploadIcon}
+      </button>
+
+      <dialog id="modal" class="dialog">
+        <button id="closeModal" class="dialog-close-btn">
+          X
+        </button>
+        <button className="button upload-csv" onClick={handleSubmit}>Choose a File</button>
+      </dialog>
+    </div>
+  );
+};
 
 export default UploadCSV;
